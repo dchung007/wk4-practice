ff97dc1606b18c9528f8748d25aed2532944f757
David Chung <dvchung00@gmail.com>
2022-04-05 14:17:23 -0700

    Finished wk4d2-Dragons-class-syntax

--- wk4d2-dragons-class-syntax/classes/dragon.js
+++ wk4d2-dragons-class-syntax/classes/dragon.js
@@ -1,4 +1,35 @@
 // Your code here
+class Dragon {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+  }
+
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`;
+  }
+
+  static getDragons(...dragons) {
+    let newArr = [];
+    dragons.forEach(dragon => {
+      newArr.push(dragon.name);
+    })
+    return newArr;
+  }
+}
+
+
+// Examples
+const puff = new Dragon("Puff", "green");
+console.log(puff);
+console.log(puff.breathesFire());
+
+const toothless = new Dragon("Toothless", "black");
+console.log(toothless);
+console.log(toothless.breathesFire());
+
+console.log(Dragon.getDragons(puff, toothless));
+
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
