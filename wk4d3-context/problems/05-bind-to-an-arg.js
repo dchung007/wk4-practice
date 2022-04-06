a77e822b45c08475c69c6c932334cc3479de872d
David Chung <dvchung00@gmail.com>
2022-04-06 14:25:25 -0700

    Finished wk4d3-context

--- wk4d3-context/problems/05-bind-to-an-arg.js
+++ wk4d3-context/problems/05-bind-to-an-arg.js
@@ -1,5 +1,7 @@
 function bindToAnArg(func, arg) {
   // Your code here
+  let boundfunc = func.bind(func, arg);
+  return boundfunc;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
